69b023c3698e11dee6715e06fc8013cc5c4af54e
Yingying Dong <37611412+KatnissD@users.noreply.github.com>
2019-04-25 11:27:05 +0800

    Update hisat2.sh

--- hisat2.sh
+++ hisat2.sh
@@ -4,8 +4,9 @@ cd /media/hp/disk1/DYY/sra_fq_RNAseq/Bacillus_subtilis/experiment1
 for item in $(ls *.sra)
 do
 	echo "hi_${item%.*}"
-	hisat2 -p 16 -t --dta -x /media/hp/disk1/DYY/reference/index/Bacillus_subtilis/genome_index 
-	-U /media/hp/disk1/DYY/sra_fq_RNAseq/Bacillus_subtilis/fastq/${item%.*}.sra.fastq.gz 
+	hisat2 -p 16 -t --dta --rna-strandness RF --max-intronlen 100000 
+	-x /media/hp/disk1/DYY/reference/index/Bacillus_subtilis/genome_index 
+	-U /media/hp/disk1/DYY/sra_fq_RNAseq/Bacillus_subtilis/experiment1/fastq/${item%.*}.sra.fastq.gz 
 	-S /media/hp/Katniss/DYY/aligned/Bacillus_subtilis/experiment1/${item%.*}.sam 
 	2>>/media/hp/Katniss/DYY/aligned/Bacillus_subtilis/experiment1/mapping_repo.txt
 	
