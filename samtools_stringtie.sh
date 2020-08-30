f4e6e958aaaf41470fb8318ac908efbf1f3ecef3
Yingying Dong <37611412+KatnissD@users.noreply.github.com>
2020-08-30 21:44:06 +0800

    Update samtools_stringtie.sh

--- samtools_stringtie.sh
+++ samtools_stringtie.sh
@@ -2,11 +2,10 @@
 
 for item in $(ls *.sam)
 do
-	echo "sam_${item%.*}"		
-	
-	samtools sort -@ 8 -o ${item%.*}.bam ${item%.*}.sam
-	species = $1
-	echo "str_${item%.*}"
-	stringtie -p 16 -G /media/hp/disk1/DYY/reference/annotation/${species}/ref.gtf -o ./${item%.*}.gtf -l ${item%.*} ${item%.*}.bam
+        echo "sam_${item%.*}"
+        samtools sort -@ 8 -o ${item%.*}.bam ${item%.*}.sam
+
+        echo "str_${item%.*}"
+        stringtie -p 30 --rf -G ./ref.gff -o ./${item%.*}.gtf -l ${item%.*} ${item%.*}.bam -A ${item%.*}_abund.out
 
 done
