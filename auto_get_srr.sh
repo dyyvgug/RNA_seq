561582e1ee3f506e12a91395cfcaa1f8fffcd5ca
Yingying Dong <37611412+dyyvgug@users.noreply.github.com>
2024-06-25 13:38:07 +0200

    Update auto_get_srr.sh

--- auto_get_srr.sh
+++ auto_get_srr.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 # Read each line in the SRR.txt file
-while IFS= read -r srr_id; do
+while IFS= read -r srr_id || [ -n "$srr_id" ]; do
   # Use the prefetch command to download each SRR file
   prefetch "$srr_id"
 done < SRR_Acc_List.txt
-
