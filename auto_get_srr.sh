470ba136ea7a6077df9ae761f8c87d0f33c3fb75
Yingying Dong <37611412+dyyvgug@users.noreply.github.com>
2024-06-25 12:05:09 +0200

    Add files via upload

---
+++ auto_get_srr.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Read each line in the SRR.txt file
+while IFS= read -r srr_id; do
+  # Use the prefetch command to download each SRR file
+  prefetch "$srr_id"
+done < SRR_Acc_List.txt
+
